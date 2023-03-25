c3e508fd9706fb9b9c675278a04092bf6778eedb
Phil <phillippelliott@gmail.com>
2023-03-25 11:45:49 +0700

    initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import { test, expect } from "vitest";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(
+    /Click on the Vite and React logos to learn more/i
+  );
+  expect(linkElement).toBeInTheDocument();
+});

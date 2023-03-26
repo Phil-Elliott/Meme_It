358e0d95ad4b86f06624f1988b06d9758bab9b18
Phil <phillippelliott@gmail.com>
2023-03-26 12:23:23 +0700

    made responsive

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -3,13 +3,13 @@ import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 const Image = () => {
   return (
-    <div className="flex items-center justify-center my-20 space-x-10 text-3xl">
-      <AiOutlineArrowLeft />
+    <div className="flex items-center justify-center my-16 space-x-4 sm:space-x-10 text-xl sm:text-3xl">
+      <AiOutlineArrowLeft className="cursor-pointer" />
       <img
-        className="w-1/2 h-1/2 object-cover object-center"
+        className="w-3/4 sm:w-1/2 h-1/2 object-cover object-center rounded"
         src="https://plus.unsplash.com/premium_photo-1667516819655-55413f299fec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
       />
-      <AiOutlineArrowRight />
+      <AiOutlineArrowRight className="cursor-pointer" />
     </div>
   );
 };
